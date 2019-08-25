150c90b9b311ebcb0b3437813196771af48124d7
Devin Shoemaker <devinshoe@gmail.com>
2019-08-25 15:56:14 -0500

    [WIP] AddTransactionPage: Karma test boilerplate

--- src/app/add-transaction/add-transaction.page.spec.ts
+++ src/app/add-transaction/add-transaction.page.spec.ts
@@ -1,5 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { IonicModule } from '@ionic/angular';
+import { of } from 'rxjs';
 
 import { AddTransactionPage } from './add-transaction.page';
 
@@ -8,11 +14,26 @@ describe('AddTransactionPage', () => {
   let fixture: ComponentFixture<AddTransactionPage>;
 
   beforeEach(async(() => {
+    const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    const angularFirestoreStub = {};
+    const angularFireAuthStub = {
+      user: {
+        subscribe() {
+          of({ userId: 'user-id' });
+        }
+      }
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ AddTransactionPage ],
+      declarations: [AddTransactionPage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-    .compileComponents();
+      imports: [ReactiveFormsModule, IonicModule],
+      providers: [
+        { provide: Router, useValue: routerSpy },
+        { provide: AngularFirestore, useValue: angularFirestoreStub },
+        { provide: AngularFireAuth, useValue: angularFireAuthStub }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
